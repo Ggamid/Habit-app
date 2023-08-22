1034f56eaec1317881f5d5721f3f3d39cdc8e5ac
Гамид Халидов <khalidov-gamid@mail.ru>
2023-08-22 15:16:40 +0300

    в Habit.swift реализованы: структура Habit, протокол HabitProtocol, enum HabitStatus

--- Habbit app/Model/Habit.swift
+++ Habbit app/Model/Habit.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+enum HabitStatus{
+    case completed, notCompleted
+}
+
+protocol HabitProtocol{
+    
+    // title of Habit
+    var title: String { get set }
+    
+    // time to do Habit
+    var time: String { get set }
+    
+    // a series of accomplished habits
+    var strick: Int { get set }
+    
+    // status of habit
+    var status: HabitStatus { get set }
+    
+}
+
+struct Habit: HabitProtocol{
+    
+    var title: String
+    var time: String
+    var strick: Int
+    var status: HabitStatus
+    
+}
---
+++ Habbit app/Model/HabitStorage.swift
@@ -0,0 +1,34 @@
+//
+//  HabitStorage.swift
+//  Habbit app
+//
+//  Created by Gamıd Khalıdov on 22.08.2023.
+//
+
+import Foundation
+
+protocol HabitStorageProtocol{
+    
+    func loadHabit() -> [Habit]
+    func saveHabit(_ habits: [Habit])
+    
+}
+
+
+class HabitStorage: HabitStorageProtocol{
+    func loadHabit() -> [Habit] {
+        let habits = [
+            Habit(title: "Drink water", time: "20:00", strick: 0, status: .notCompleted),
+            Habit(title: "Sport", time: "15:00", strick: 0, status: .notCompleted),
+            Habit(title: "Book", time: "13:00", strick: 0, status: .notCompleted),
+            Habit(title: "Long named Habit for test", time: "13:00", strick: 0, status: .notCompleted)
+        ]
+        return habits
+    }
+    
+    func saveHabit(_ habits: [Habit]) {
+        
+    }
+    
+    
+}
